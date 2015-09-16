d33b8d14ff18937180db28920ee4adccca4c9da7
John Connolly <jeconnol@gmail.com>
2015-09-15 23:13:03 -0400

    move stub javascript into edit.js

--- src/main/resources/assets/webApp/edit.js
+++ src/main/resources/assets/webApp/edit.js
@@ -1,9 +1,9 @@
 $(document).ready(
-        
+  $("#save").click(function () {
+    var textAreaValue = $("#body").text();
+    alert(textAreaValue); //do POST to /API/{{title}} here
+  });
 );
 
-function setHeader() {
-    
-}
 
 
